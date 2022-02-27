2574a1c810971d550628cf26d09d4136cb9cc296
Thiago <thiago@jeraapps.com>
2022-02-27 14:49:33 -0300

    new loading component

--- presentation/src/main/java/com/example/todoapp/util/components/LoadingComponent.kt
+++ presentation/src/main/java/com/example/todoapp/util/components/LoadingComponent.kt
@@ -1,15 +1,21 @@
 package com.example.todoapp.util.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.example.todoapp.ui.theme.DarkBlue
+import com.example.todoapp.ui.theme.RobotoRegular
 
 @Composable
 fun LoadingComponent() {
@@ -19,9 +25,26 @@ fun LoadingComponent() {
             .background(color = Color.Black.copy(alpha = 0.3f))
             .zIndex(1f)
     ) {
-        CircularProgressIndicator(
-            color = DarkBlue,
-            modifier = Modifier.align(Alignment.Center),
-        )
+        Card(
+            modifier = Modifier
+                .align(Center)
+                .size(height = 150.dp, width = 200.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+                horizontalAlignment = CenterHorizontally
+            ) {
+                CircularProgressIndicator(
+                    color = DarkBlue,
+                    modifier = Modifier.align(CenterHorizontally),
+                )
+                Text(
+                    text = "Loading...",
+                    fontFamily = RobotoRegular,
+                    modifier = Modifier.padding(top = 8.dp),
+                )
+            }
+        }
+
     }
 }
